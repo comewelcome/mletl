4fe7a2f49f2b6d45566770f5afddd41878e898b3
haouach <mohammed.haouach@cnav.fr>
2024-11-08 14:58:45 +0100

    avec kolo

--- mletl/settings.py
+++ mletl/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "kolo.middleware.KoloMiddleware",
 ]
 
 ROOT_URLCONF = 'mletl.urls'
